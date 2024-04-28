292a389e5cc98ea65280a4d919084b64fd8f8635
ChaisRobinson <chais5robinson@gmail.com>
2024-04-28 16:29:50 -0400

    Temp commit

--- controllers/vegetablesController.js
+++ controllers/vegetablesController.js
@@ -18,20 +18,20 @@ const getAllVegetables = async (req, res) => {
   //Get Vegetable by ID
   const getVegetableById = async (req, res) => {
     try {
-      // Validate the provided fruitID
+      // Validate the provided vegtableID
       const vegtableID = req.params.id;
       if (!vegtableID) {
         return res.status(400).json({ error: "Vegtable ID is required" });
       }
   
-      // Find the fruit by ID
+      // Find the vegtable by ID
       const vegtable = await Vegtable.findById(vegtableID);
   
-      // Check if the fruit exists
+      // Check if the vegtable exists
       if (!vegtable) {
         return res.status(404).json({ error: "Vegtable not found" });
       }
-      // Send the fruit document as a JSON response
+      // Send the vegtable document as a JSON response
       res.json(vegtable);
     } catch (err) {
       console.error(err);
